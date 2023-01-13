11612c8f9d8ee77fc079adcce0b50e6319e38399
Deniffler <denis7400@gmail.com>
2023-01-14 04:17:19 +0500

    Обновил 2 проект

--- Example_002_HelloUser/Program.cs
+++ Example_002_HelloUser/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите Ваше Имя: ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
