d003470de550ea76222395da3e239330d4650d65
Deniffler <denis7400@gmail.com>
2023-01-14 15:06:44 +0500

    Поиск максимального числа

--- Example_006_Base/Program.cs
+++ Example_006_Base/Program.cs
@@ -4,23 +4,12 @@ int c = new Random().Next(1,101);
 int d = new Random().Next(1,101);
 int e = new Random().Next(1,101);
 int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
 
-if (b > max)
-{
-    max = b;
-}
-if (c > max)
-{
-    max = c;
-}
-if (d > max)
-{
-    max = d;
-}
-if (e > max)
-{
-    max = e;
-}
 Console.WriteLine("Введены следующие числа: ");
 Console.Write(a);
 Console.Write(", ");
