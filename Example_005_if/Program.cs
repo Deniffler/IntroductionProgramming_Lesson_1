a29ee91ef47a3f7254c31f14cd64b648fd7562e0
Deniffler <denis7400@gmail.com>
2023-01-14 14:36:57 +0500

    Программа ветвления

---
+++ Example_005_if/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine("Привет Маша! Маша с УралМаша"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
